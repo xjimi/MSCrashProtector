adf4893afaf3983c1fd0ea11529cf31f2a82b92a
“jiang.zijia@immomo.com” <jiang.zijia@immomo.com>
2018-02-26 22:40:16 +0800

    init.

---
+++ MSCrashDemo/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  MSCrashDemo
+//
+//  Created by J on 2018/2/26.
+//  Copyright © 2018年 J. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+}
+
+
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
+    
+    
+}
+
+@end
